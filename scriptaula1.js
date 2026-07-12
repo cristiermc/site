d910ebfd0adf563aaec02ffb1773f955c5ebe773
Cristier Machado Cittadin <cristiermc@gmail.com>
2026-07-11 23:04:51 -0300

    update script aula1
    classes

--- scriptaula1.js
+++ scriptaula1.js
@@ -1,16 +1,59 @@
-class Carro{
-    constructor(marca){
-        this.marca = marca; //propriedade da classe
+class Veiculo{
+    constructor(fabricante,marca,ano,tipo,qtdPortas){
+        this.fabricante = fabricante;
+        this.marca = marca;
+        this.ano = ano;
+        this.tipo = tipo;
+        this.qtdPortas = qtdPortas;
     }
-    mostrarMarca(){
-        console.log('A marca do carro é '+this.marca);
+    mostrarDadosDoVeiculo(){
+        console.log(`${this.fabricante} ${this.marca} ${this.ano} ${this.tipo} ${this.qtdPortas} portas`);
     }
 }
 
-const meuCarro = new Carro("Ford");
+class Moto extends Veiculo{
+    constructor(fabricante,marca,ano){
+        super(fabricante,marca,ano);
+    }
+    mostrarDadosDoVeiculo(){
+        console.log(`${this.fabricante} ${this.marca} ${this.ano}`)
+    }
 
-meuCarro.mostrarMarca();
+}
 
-let array1 = [1,2,3,4,5];
-console.log(array1.sort());
-console.log(array1.length);
+class Carro extends Veiculo{
+    constructor(fabricante,marca,ano,tipo,qtdPortas){
+        super(fabricante,marca,ano,tipo,qtdPortas);
+    }
+    mostrarDadosDoVeiculo(){
+        console.log(`${this.fabricante} ${this.marca} ${this.ano} ${this.tipo} ${this.qtdPortas} portas`);
+    }
+}
+
+// class Carro{
+//     constructor(fabricante, marca,ano,tipo,qtdPortas){
+//         this.fabricante = fabricante; //propriedade da classe
+//         this.marca = marca;
+//         this.qtdPortas = qtdPortas;
+//         this.tipo = tipo;
+//         this.ano = ano;
+//     }
+//     mostrarDadosDoCarro(){
+//         console.log(this.fabricante+' '+this.marca+' '+this.qtdPortas+' '+this.tipo+' '+this.ano);
+//         console.log(`${this.fabricante} ${this.marca} ${this.qtdPortas} ${this.tipo} ${this.ano}`);
+
+//     }
+
+//     mostrarMarca(){
+//         console.log(`O modelo do carro é ${this.marca}`);
+//     }
+//     acelerar(){
+//         console.log('Acelerando...');
+//     }
+// }
+ const meuCarro = new Veiculo("Ford", "ka", "2000", "sedan", 4);
+ const minhaMoto = new Moto("");
+ 
+ meuCarro.mostrarDadosDoVeiculo();
+//  meuCarro.mostrarMarca();
+//  meuCarro.acelerar();
