47a236d6af9f67a913b6446179847ccb3d210023
Cristier Machado Cittadin <cristiermc@gmail.com>
2026-07-12 15:16:00 -0300

    update scriptaula3
    assicronia

--- scriptaula3.js
+++ scriptaula3.js
@@ -1,11 +1,26 @@
-function alerta(){
-    console.log("Alerta");
+console.log("Inicio da execução");
+
+function buscaDadosServidor (){
+    //codigo que busca os dados do servidor
+    return new Promise((resolve,reject) => {
+        console.log("Buscando dados no servidor");
+
+        setTimeout(()=>{
+            let sucesso = Math.random() > 0.5;
+
+            if(sucesso){
+                resolve("Dados recebidos com sucesso");
+            } else {
+                reject("Falha ao buscar dados")
+            }
+        }, 2000);
+    })
 }
 
-setTimeout(alerta, 3000)
-setTimeout(() => {
-    console.log("alerta 2");},3000);
+buscaDadosServidor().then((mensagem) => {
+    console.log(mensagem);
+}).catch((erro) =>{
+    console.error(erro);
+});
 
-setInterval(() => {
-    console.log("Alerta Interval");
-}, 2000);
+console.log("Fim da execução");
